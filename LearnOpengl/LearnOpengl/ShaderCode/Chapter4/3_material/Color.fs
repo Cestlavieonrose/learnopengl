dc7ca7b18f88cc982e7a919b8c97ff6a91ae1d9a
cestlavieonrose <xtu@zju.edu.cn>
2021-05-03 01:04:39 +0800

    4.3 材质- 灯光属性- 完成

--- LearnOpengl/LearnOpengl/ShaderCode/Chapter4/3_material/Color.fs
+++ LearnOpengl/LearnOpengl/ShaderCode/Chapter4/3_material/Color.fs
@@ -8,12 +8,21 @@ struct Material {
 
 uniform Material material;
 
+struct Light {
+    vec3 position;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform Light light;
+
 
 out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 
@@ -24,18 +33,18 @@ in vec3 Normal;
 void main()
 {
     //环境光
-    vec3 ambient = lightColor * material.ambient;
+    vec3 ambient = light.ambient * material.ambient;
     //漫反射
-    vec3 lightDir = normalize(lightPos-FragPos);
+    vec3 lightDir = normalize(light.position-FragPos);
     vec3 norm = normalize(Normal);
     float diff = max(dot(norm, lightDir), 0);
-    vec3 diffColor = lightColor * (diff * material.diffuse);
+    vec3 diffColor = light.diffuse * (diff * material.diffuse);
     
     //高光反射
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular =  lightColor * (spec * material.specular);
+    vec3 specular =  light.specular * (spec * material.specular);
 
     vec3 result = (ambient + diffColor + specular);
     FragColor = vec4(result, 1.0);
