f05072216617dddd3b2d7e58a27daf0396364ede
cestlavieonrose <xtu@zju.edu.cn>
2021-05-02 23:17:34 +0800

    4.1 颜色 - 完成

--- LearnOpengl/LearnOpengl/Utils/Camera/Camera.hpp
+++ LearnOpengl/LearnOpengl/Utils/Camera/Camera.hpp
@@ -1,14 +1,192 @@
-//
-//  Camera.hpp
-//  LearnOpengl
-//
-//  Created by 屠讯南 on 2021/5/2.
-//  Copyright © 2021 colortomorrow. All rights reserved.
-//
-
 #ifndef Camera_hpp
 #define Camera_hpp
 
-#include <stdio.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
-#endif /* Camera_hpp */
+#include <vector>
+
+// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
+enum Camera_Movement {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT
+};
+
+// Default camera values
+//以x轴为参考点为0度，而相机front为z轴，因此为-90度
+const float YAW         = -90.0f;
+const float PITCH       =  0.0f;
+//敏感度系数,wasd键控制相机移动的速度
+const float SPEED       =  2.5f;
+//敏感度系数,鼠标控制旋转角度的速度
+const float SENSITIVITY =  0.1f;
+//这个通过鼠标滚轮控制
+const float ZOOM        =  45.0f;
+
+
+// An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
+class Camera
+{
+public:
+    // 相机坐标系原点在世界坐标系中的位置
+    glm::vec3 Position;
+    //这个是相机坐标系的-z轴
+    glm::vec3 Front;
+    //相机坐标系的y轴
+    glm::vec3 Up;
+    //相机坐标系的x轴
+    glm::vec3 Right;
+    //世界坐标系的y轴
+    glm::vec3 WorldUp;
+    // 俯仰角，即当前相机坐标系的-z轴在世界坐标系中和世界坐标系y轴的夹角
+    float Yaw;
+    //偏转角，即当前相机坐标系的x轴在世界坐标系中和世界坐标系x轴的夹角
+    float Pitch;
+    // camera options
+    float MovementSpeed;
+    float MouseSensitivity;
+    float Zoom;
+
+    // constructor with vectors
+    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = position;
+        WorldUp = up;
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+    // constructor with scalar values
+    Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    {
+        Position = glm::vec3(posX, posY, posZ);
+        WorldUp = glm::vec3(upX, upY, upZ);
+        Yaw = yaw;
+        Pitch = pitch;
+        updateCameraVectors();
+    }
+
+    // 计算相机的lookat矩阵
+    glm::mat4 GetViewMatrix()
+    {
+        glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+        translation[3][0] = -Position.x; // Third column, first row
+        translation[3][1] = -Position.y;
+        translation[3][2] = -Position.z;
+        glm::mat4 rotation = glm::mat4(1.0f);
+        rotation[0][0] = Right.x; // First column, first row
+        rotation[1][0] = Right.y;
+        rotation[2][0] = Right.z;
+        rotation[0][1] = Up.x; // First column, second row
+        rotation[1][1] = Up.y;
+        rotation[2][1] = Up.z;
+        rotation[0][2] = -Front.x; // First column, third row
+        rotation[1][2] = -Front.y;
+        rotation[2][2] = -Front.z;
+        
+        //glm::mat4 view2 = rotation * translation;
+
+       // return glm::lookAt(Position, Position + Front, WorldUp);
+       // return glm::transpose(view);
+        return rotation * translation;
+       // return view;
+    }
+    
+    glm::mat4 calculate_lookAt_matrix(glm::vec3 position, glm::vec3 target, glm::vec3 worldUp)
+    {
+        // 1. Position = known
+        // 2. Calculate cameraDirection
+        glm::vec3 zaxis = glm::normalize(position - target);
+        // 3. Get positive right axis vector
+        glm::vec3 xaxis = glm::normalize(glm::cross(glm::normalize(worldUp), zaxis));
+        // 4. Calculate camera up vector
+        glm::vec3 yaxis = glm::cross(zaxis, xaxis);
+
+        // Create translation and rotation matrix
+        // In glm we access elements as mat[col][row] due to column-major layout
+        glm::mat4 translation = glm::mat4(1.0f); // Identity matrix by default
+        translation[3][0] = -position.x; // Third column, first row
+        translation[3][1] = -position.y;
+        translation[3][2] = -position.z;
+        glm::mat4 rotation = glm::mat4(1.0f);
+        rotation[0][0] = xaxis.x; // First column, first row
+        rotation[1][0] = xaxis.y;
+        rotation[2][0] = xaxis.z;
+        rotation[0][1] = yaxis.x; // First column, second row
+        rotation[1][1] = yaxis.y;
+        rotation[2][1] = yaxis.z;
+        rotation[0][2] = zaxis.x; // First column, third row
+        rotation[1][2] = zaxis.y;
+        rotation[2][2] = zaxis.z;
+
+        // Return lookAt matrix as combination of translation and rotation matrix
+        return rotation * translation; // Remember to read from right to left (first translation then rotation)
+    }
+
+    // processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+    void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+    {
+        float velocity = MovementSpeed * deltaTime;
+        if (direction == FORWARD)
+            Position += Front * velocity;
+        if (direction == BACKWARD)
+            Position -= Front * velocity;
+        if (direction == LEFT)
+            Position -= Right * velocity;
+        if (direction == RIGHT)
+            Position += Right * velocity;
+    }
+
+    // processes input received from a mouse input system. Expects the offset value in both the x and y direction.
+    void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+    {
+        xoffset *= MouseSensitivity;
+        yoffset *= MouseSensitivity;
+
+        Yaw   += xoffset;
+        Pitch += yoffset;
+
+        // make sure that when pitch is out of bounds, screen doesn't get flipped
+        if (constrainPitch)
+        {
+            if (Pitch > 89.0f)
+                Pitch = 89.0f;
+            if (Pitch < -89.0f)
+                Pitch = -89.0f;
+        }
+
+        // update Front, Right and Up Vectors using the updated Euler angles
+        updateCameraVectors();
+    }
+
+    // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+    void ProcessMouseScroll(float yoffset)
+    {
+        Zoom -= (float)yoffset;
+        if (Zoom < 1.0f)
+            Zoom = 1.0f;
+        if (Zoom > 45.0f)
+            Zoom = 45.0f;
+    }
+
+private:
+    // calculates the front vector from the Camera's (updated) Euler Angles
+    void updateCameraVectors()
+    {
+        // calculate the new Front vector
+        glm::vec3 front;
+        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        front.y = sin(glm::radians(Pitch));
+        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+        //front本来就是一个单位向量，没必要normalize
+        Front = front;
+        // also re-calculate the Right and Up vector
+        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
+        //Up不需要计算 直接用worldUp就可以
+        Up    = glm::cross(Right, Front);
+    }
+};
+#endif
