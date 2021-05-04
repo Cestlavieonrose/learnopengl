93ee98d3d2159c270574d3ab407e597c03ddb687
cestlavieonrose <xtu@zju.edu.cn>
2021-05-04 21:46:26 +0800

    6.1 深度测试- 深度冲突

--- LearnOpengl/LearnOpengl/ShaderCode/Chapter6/1_depthtest/frag.fs
+++ LearnOpengl/LearnOpengl/ShaderCode/Chapter6/1_depthtest/frag.fs
@@ -6,7 +6,7 @@ in vec2 TexCoords;
 uniform sampler2D texture1;
 
 float near = 0.1;
-float far  = 100.0; 
+float far  = 100.0;
 float LinearizeDepth(float depth)
 {
     float z = depth * 2.0 - 1.0; // back to NDC
@@ -15,6 +15,6 @@ float LinearizeDepth(float depth)
 
 void main()
 {
-    float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 farr
-    FragColor = vec4(vec3(depth), 1.0);
+   // float depth = LinearizeDepth(gl_FragCoord.z) / far; // 为了演示除以 farr
+    FragColor = vec4(texture(texture1, TexCoords).rgb, 1.0);
 }
