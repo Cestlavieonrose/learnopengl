9e1554f4fd77c0d59f2d22ca511f674280a360a3
cestlavieonrose <xtu@zju.edu.cn>
2021-05-07 01:39:19 +0800

    6.5 帧缓冲 - 锐化- 完成

--- LearnOpengl/LearnOpengl/main.cpp
+++ LearnOpengl/LearnOpengl/main.cpp
@@ -77,7 +77,7 @@ int main()
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     
     Shader shader("ShaderCode/Chapter6/4_framebuffer/framebuffers.vs", "ShaderCode/Chapter6/4_framebuffer/framebuffers.fs");
-    Shader screenShader("ShaderCode/Chapter6/4_framebuffer/framebuffers_screen.vs", "ShaderCode/Chapter6/4_framebuffer/framebuffers_screen_gray.fs");
+    Shader screenShader("ShaderCode/Chapter6/4_framebuffer/framebuffers_screen.vs", "ShaderCode/Chapter6/4_framebuffer/framebuffers_screen_ Convolution_Ruihua.fs");
     
     // configure global opengl state
     // -----------------------------
